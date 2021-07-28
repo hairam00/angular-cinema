062d7f222122f9ddc5a50acaf36fd9c39fb86e52
Hairam Irshad <hairamirsahd97@gmail.com>
2021-07-28 14:48:05 +0500

    first commit

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {FormControl, FormGroup, FormGroupDirective, Validators} from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { RestService } from 'src/app/Services/rest.service';
 
@@ -11,19 +11,27 @@ import { RestService } from 'src/app/Services/rest.service';
 export class FooterComponent implements OnInit {
   whatsappCity = 'option2';
   newsLetterCity = 'option2';
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
-  newsCity = new FormControl('', [
-    Validators.required
-  ]);
-  whatsappsCity = new FormControl('',[
-    Validators.required
-  ]);
-  whatsappNumber = new FormControl('',[
-    Validators.required
-  ]);
+  whatsappForm = new FormGroup({
+    showTimes : new FormControl('',[]),
+    upcoming : new FormControl('',[]),
+    blogs : new FormControl('',[]),
+    whatsappsCity : new FormControl('',[
+      Validators.required
+    ]),
+    whatsappNumber : new FormControl('',[
+      Validators.required
+    ])
+  });
+  newsForm = new FormGroup({
+    emailFormControl : new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    newsCity : new FormControl('', [
+      Validators.required
+    ])  
+  });
+  
   newsLetter = {
     id: 0,
     email: '',
@@ -44,7 +52,7 @@ export class FooterComponent implements OnInit {
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action);
   }
-  createNews(){
+  createNews(newsForm: FormGroupDirective){
     const data = {
       id: this.newsLetter.id,
       email: this.newsLetter.email,
@@ -64,10 +72,17 @@ export class FooterComponent implements OnInit {
           console.log(error);
         });
         this.openSnackBar("Subscribe Successful", "x");
+        newsForm.resetForm();
+        this.newsForm.get('newsCity')?.clearValidators();
+        this.newsForm.get('emailFormControl')?.clearValidators();
+        this.newsForm.get('newsCity')?.setValue('');
+        this.newsForm.get('emailFormControl')?.setValue('');    
+        this.newsForm.controls['newsCity'].setValidators([Validators.required]);
+        this.newsForm.controls['emailFormControl'].setValidators([Validators.required]);     
     }
     
   }
-  createWhatsapp(){
+  createWhatsapp(formDirective: FormGroupDirective){
     const whatsappData = {
       id: this.whatsapp.id,
       number: this.whatsapp.number,
@@ -90,6 +105,15 @@ export class FooterComponent implements OnInit {
           console.log(error);
         });
         this.openSnackBar("Whatsapp Subscription Successfull", "x");
+        formDirective.resetForm();
+        this.whatsappForm.get('whatsappNumber')?.clearValidators();
+        this.whatsappForm.get('whatsappsCity')?.clearValidators();
+        this.whatsappForm.get('whatsappsCity')?.setValue('');
+        this.whatsappForm.get('whatsappNumber')?.setValue(''); 
+        this.whatsappForm.get('whatsappsCity')?.setValidators([Validators.required]);
+        this.whatsappForm.get('whatsappNumber')?.setValidators([Validators.required]);     
+        // this.whatsappForm.controls['whatsappsCity'].setValidators([Validators.required]);
+        // this.whatsappForm.controls['whatsappNumber'].setValidators([Validators.required]);     
     }
     
   }

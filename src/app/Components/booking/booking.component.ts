523706fa815afad03d2368319f6de6a67b7f7aea
Hairam Irshad <hairamirsahd97@gmail.com>
2021-08-02 17:29:14 +0500

    first commit

--- src/app/Components/booking/booking.component.ts
+++ src/app/Components/booking/booking.component.ts
@@ -13,6 +13,14 @@ id: any;
 fromPage: string = '';
 fromDialog: string = '';
 movieData : any;
+selectedTime: string = '';
+// status: boolean = false;
+slot1: boolean = false;
+slot2: boolean = false;
+slot3: boolean = false;
+slot4: boolean = false;
+slot5: boolean = false;
+slot6: boolean = false;
 
 bookDate: string = '';
 seats: number = 0;
@@ -22,6 +30,8 @@ seats: number = 0;
     }
 
   ngOnInit(): void {
+    console.log(this.selectedTime);
+    
   }
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action);
@@ -29,17 +39,81 @@ seats: number = 0;
   getMovieByID(id:string){
     this.rest.getMovie(id).subscribe(res => {
       this.movieData = res[0];
-      console.log(this.movieData);
-    })
+    });
   }
   uploadBooking(date:string, seats:string){
     let data: any = {
       booking_date: date,
       seats: seats,
-      ticket_id: this.movieData.id
+      ticket_id: this.movieData.id,
+      timings: this.selectedTime
     };
-    this.rest.uploadBooking(data).subscribe();
-    this.openSnackBar(seats+ " Seats booked for "+ date,"x");
+    if(this.selectedTime == ''){
+      this.openSnackBar("Please Select Time slot","x");
+    }else if(date == ''){
+      this.openSnackBar("Please select Date", "x");
+    }else if(seats == ''){
+      this.openSnackBar("Please select seats", "x");
+    }else{
+      this.rest.uploadBooking(data).subscribe();
+      this.openSnackBar(seats+ " Seats booked for "+ date + " at "+ data.timings,"x");
+    }
   }
-
+  selectTime(slot:string){
+    this.selectedTime = slot;
+    // this.status = !this.status; 
+    console.warn(this.selectedTime);
+  }
+  toggle(arr:any){
+    
+    console.log(arr);
+    if(arr == 'slot1'){
+      if(this.slot1 == true){
+        document.getElementById(arr)?.classList.remove(arr)  
+      }else{
+        document.getElementById(arr)?.classList.add(arr);
+      }
+      this.slot1=!this.slot1;
+    }
+    if(arr == 'slot2'){
+      if(this.slot2 == true){
+        document.getElementById(arr)?.classList.remove(arr)  
+      }else{
+        document.getElementById(arr)?.classList.add(arr);
+      }
+      this.slot2 =!this.slot2;
+    }
+    if(arr == 'slot3'){
+      if(this.slot3 == true){
+        document.getElementById(arr)?.classList.remove(arr)  
+      }else{
+        document.getElementById(arr)?.classList.add(arr);
+      }
+      this.slot3 =!this.slot3;
+    }
+    if(arr == 'slot4'){
+      if(this.slot4 == true){
+        document.getElementById(arr)?.classList.remove(arr)  
+      }else{
+        document.getElementById(arr)?.classList.add(arr);
+      }
+      this.slot4 =!this.slot4;
+    }
+    if(arr == 'slot5'){
+      if(this.slot5 == true){
+        document.getElementById(arr)?.classList.remove(arr)  
+      }else{
+        document.getElementById(arr)?.classList.add(arr);
+      }
+      this.slot5 =!this.slot5;
+    }
+    if(arr == 'slot6'){
+      if(this.slot6 == true){
+        document.getElementById(arr)?.classList.remove(arr)  
+      }else{
+        document.getElementById(arr)?.classList.add(arr);
+      }
+      this.slot6 =!this.slot6
+    }
+  }
 }

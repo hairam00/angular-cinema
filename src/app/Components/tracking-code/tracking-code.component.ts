164f940a88e34ca682420fa06a8424db9db5d1ce
Hairam Irshad <hairamirsahd97@gmail.com>
2021-08-03 12:10:10 +0500

    first commit

--- src/app/Components/tracking-code/tracking-code.component.ts
+++ src/app/Components/tracking-code/tracking-code.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from 'src/app/Services/rest.service';
 
 @Component({
   selector: 'app-tracking-code',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrackingCodeComponent implements OnInit {
   value = '';
-  constructor() { }
+  bookData:Array<any>= [];
+  constructor(private rest:RestService) { }
 
   ngOnInit(): void {
+    this.getBookings();
   }
-
+  getBookings(){
+    this.rest.getBookings().subscribe(response =>{
+      for(let i=0; i < response.length;i++){
+        this.getMovieById(response[i]["ticket_id"]);
+      }
+      console.warn(this.bookData);
+    })
+  }
+  getMovieById(id:string){
+    this.rest.getMovie(id).subscribe(resp => {
+      this.bookData.push(resp)
+    })
+  }
 }

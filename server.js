35b10bcda58dbf05b14c65d62084ada800bd3c04
Hairam Irshad <hairamirsahd97@gmail.com>
2021-08-12 17:10:19 +0500

    first commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/bookitnow/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+// Start the app 
 app.listen(process.env.PORT || 8080);
 // server.js
 const jsonServer = require('json-server')

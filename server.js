306ea793d3219f9aa7d11c26efffb89e94b1b990
Hairam Irshad <hairamirsahd97@gmail.com>
2021-08-12 17:00:05 +0500

    first commit

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ res.sendFile(path.join(__dirname+'/dist/bookitnow/index.html'));
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 // server.js
-// const jsonServer = require('json-server')
-// const server = jsonServer.create()
-// const router = jsonServer.router('../bookitnow/db.json')
-// const middlewares = jsonServer.defaults()
- 
-// server.use(middlewares)
-// server.use(router)
-// server.listen(process.env.PORT || 3000, () => {
-//   console.log('JSON Server is running')
-// })
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})

123ea28baa7e379c4bd7dcb9b6131edb15737e90
Hairam Irshad <hairamirsahd97@gmail.com>
2021-08-13 15:10:07 +0500

    first commit

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+import express, { static } from 'express';
+import { join } from 'path';
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/bookitnow'));
+app.use(static(__dirname + '/dist/bookitnow'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/bookitnow/index.html'));
+res.sendFile(join(__dirname+'/dist/bookitnow/index.html'));
 });
 
 // Start the app 
 app.listen(process.env.PORT || 8080);
 // server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+import { create, router as _router, defaults } from 'json-server';
+const server = create()
+const router = _router('db.json')
+const middlewares = defaults()
 
 server.use(middlewares)
 server.use(router)
